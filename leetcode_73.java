5dc0a309ded81aec729e62573d5ee9f0916f1264
yjc <55907674+919106840541@users.noreply.github.com>
2024-07-30 17:22:26 +0800

    leetcode 73.矩阵置零 （中等）

---
+++ leetcode_73.java
@@ -0,0 +1,44 @@
+//抄的力扣题解 太离谱了 脑子都被搞乱了 
+//用矩阵的第一行与第一列标记某一列或某一行的元素需要变为0
+//但这样原数组的第一行和第一列别修改 无法记录它们是否原本包含0
+//因此需要额外两个变量标记第一行和第一列是否原本包含0
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean flagCol0 = false, flagRow0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol0 = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                flagRow0 = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (flagCol0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (flagRow0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
