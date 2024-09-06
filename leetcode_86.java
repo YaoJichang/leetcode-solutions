bf4328c6a542f02d2d2ad432cee749425ab6ff75
yjc <55907674+919106840541@users.noreply.github.com>
2024-09-06 14:25:50 +0800

    leetcode 86.分隔链表 （中等）

---
+++ leetcode_86.java
@@ -0,0 +1,22 @@
+//抄的题解 用两个链表存储小值与大值 最后把小的表插到大的表就行（这种思路最简洁）
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+}
